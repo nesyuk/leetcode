592d221ffb8437202647147d0ae39d3907ffc88b
Nelly Kuznetsova <nelly@fwd.be>
2021-09-02 12:56:37 +0200

    Add postorder traversal with single stack

--- src/bstree/traversal/postorder_traversal_iterative_stack.go
+++ src/bstree/traversal/postorder_traversal_iterative_stack.go
@@ -5,37 +5,40 @@ import (
 	"bstree"
 )
 
-func postorderTraversalStacks(root *bstree.TreeNode) []int {
+func postorderTraversalStack(root *bstree.TreeNode) []int {
 	result := []int{}
-	if root == nil {
-		return result
-	}
-	stack := []*bstree.TreeNode{root}
-	seen := []*bstree.TreeNode{}
-	for len(stack) != 0 {
-		current := stack[len(stack)-1]
+	stack := []*bstree.TreeNode{}
+	current := root
+	for {
+		for current != nil {
+			stack = append(stack, current)
+			stack = append(stack, current)
+			current = current.Left
+		}
+
+		if len(stack) == 0 {
+			return result
+		}
+
+		current = stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		seen = append(seen, current)
-		if current.Left != nil {
-			stack = append(stack, current.Left)
+		if len(stack) != 0 && stack[len(stack)-1] == current {
+			current = current.Right
+		} else {
+			result = append(result, current.Val)
+			current = nil
 		}
-		if current.Right != nil {
-			stack = append(stack, current.Right)
-		}
-	}
-	for i := len(seen)-1; i >= 0; i-- {
-		result = append(result, seen[i].Val)
 	}
 	return result
 }
 
 func run() {
 	fmt.Println("go!")
-	bs := bstree.NewTree([]int{2, 1, 3})
-	fmt.Println(postorderTraversalStacks(bs))
+	bs := bstree.NewTree([]int{1, 2, 3, 5})
+	fmt.Println(postorderTraversalStack(bs))
 
 	bs = bstree.NewTree([]int{5, 6, 3, 1, 5, 7, 8, 9, 2})
-	fmt.Println(postorderTraversalStacks(bs))
+	fmt.Println(postorderTraversalStack(bs))
 }
 
 func main() {
