28bdad6bd00c369a3344a12e0b290a7e4afff29f
Nelly Kuznetsova <nelly@fwd.be>
2021-10-02 12:28:22 +0200

    Add negative marking

---
+++ src/array/negative-marking.go
@@ -0,0 +1,19 @@
+
+// all n in nums are in [1...n]
+func findDuplicate(nums []int) int {
+    for _, n := range nums {
+        // treat number as an index, check if we visited number at this index or not
+        // use "negative marking" to set number as "visited"
+        idx := n
+        if idx < 0 {
+            idx *= -1
+        }
+        if nums[idx] < 0 {
+            // index was visited, so return the number
+            return idx
+        }
+        // mark as visited
+        nums[idx] = -1 * nums[idx]
+    }
+    return -1
+}
